7e3da7d00e9f4aed86368d3ba2c2b84085881fbd
Luis Mendes <lmmendes@gmail.com>
2025-08-10 21:51:21 +0100

    chore: updated text

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -3,9 +3,10 @@
         <div
             class="mx-auto max-w-6xl px-4 py-8 text-center text-sm text-neutral-600"
         >
-            <p>Feito com ❤️ por Luís &amp; Sónia ’26</p>
+            <p>Feito com ❤️ por  Sónia & Luís’26</p>
         </div>
     </footer>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
